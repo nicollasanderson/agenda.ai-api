643421fbbce8897d05c4834b697e1ae0a20d3852
Nicollas Anderson <nicollas_sdias@hotmail.com>
2022-12-06 16:17:04 -0300

    fix: fixed db configs for deploy

--- agendaai/settings.py
+++ agendaai/settings.py
@@ -90,14 +90,25 @@ WSGI_APPLICATION = "agendaai.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.getenv("POSTGRES_DB"),
+#         "USER": os.getenv("POSTGRES_USER"),
+#         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+#         "HOST": "0.0.0.0",
+#         "PORT": 5432,
+#     }
+# }
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_DB"),
-        "USER": os.getenv("POSTGRES_USER"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": "0.0.0.0",
-        "PORT": 5432,
+        "NAME": "<PGDATABASE>",
+        "USER": "<PGUSER>",
+        "PASSWORD": "<PGPASSWORD>",
+        "HOST": "<PGHOST>",
+        "PORT": "<PGPORT>",
     }
 }
 
